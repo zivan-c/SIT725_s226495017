e709144d94d3060c323bc0fd1f9b89ce496f2ac8
zivan-c <zivancode@gmail.com>
2026-07-28 11:44:24 +1000

    Redid my P3 for modal support

--- 4.2P/public/js/scripts.js
+++ 4.2P/public/js/scripts.js
@@ -1,3 +1,55 @@
-const clickMe = () => { alert("Lebron will be going back to Cleveland.")};
-$(document).ready(function(){$('.materialboxed').materialbox();
-$('#clickMeButton').click(()=>{clickMe();})});
+const cardList = [
+    {
+    title: "goat",
+    image: "images/jr.jpeg",
+    link: "lebrawn",
+    desciption: "embiid or bron"
+    },
+    {
+    title: "cant believe",
+    image: "images/cant.jpeg",
+    link: "cant believe",
+    desciption: "cant believe this is my life rn"
+    }
+]
+
+const clickMe = () => {
+    alert("chip or nah?")
+}
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#clickMeButton').click(()=>{clickMe();})
+});
+
+const addCards = (items) => {items.forEach(item => {
+let itemToAppend = '<div class="col s4 center-align">'+
+'<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+'</div><div class="card-content">'+
+'<span class="card-title activator white-text">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+'<div class="card-reveal">'+
+'<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+'<p class="card-text">'+item.desciption+'</p>'+
+'</div></div></div>';
+$("#card-section").append(itemToAppend)
+});}
+
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+    console.log("Form Data Submitted: ", formData);
+}
+
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('.modal').modal();
+    $('#clickMeButton').click(()=>{
+    clickMe();
+    })
+    $('#formSubmit').click(() => {submitForm();})
+    addCards(cardList);
+});
--- 4.2P/server.js
+++ 4.2P/server.js
@@ -5,8 +5,15 @@ app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+app.get('/api/projects',(req,res) => {
+res.json({statusCode: 200, data: cardList, message:"Success"})
+})
+
 var port = process.env.port || 3000;
 
 app.listen(port,()=>{
 console.log("App listening to: "+port)
 });
+
+
